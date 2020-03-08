95cd02bc9646fb2a52ffb301d03272ca68efc6c1
josemagb23 <jmgallardob98@gmail.com>
2020-03-08 22:33:28 +0100

    Funcion MAC

--- src/mac/MAC.java
+++ src/mac/MAC.java
@@ -2,18 +2,36 @@
 package mac;
 
 import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
 public class MAC {
 
-	public static void main(final String[] args) {
-		System.out.println("Token: " + MAC.generateRandomText());
+	public static void main(final String[] args) throws NoSuchAlgorithmException {
+
+		String hash = MAC.generateHash("Esto es una prueba");
+		String token = MAC.generateToken();
+
+		System.out.println("MAC: " + token + "" + hash);
 
 	}
 
-	public static String generateRandomText() {
+	public static String generateHash(final String text) throws NoSuchAlgorithmException {
+		MessageDigest md = MessageDigest.getInstance("SHA-256");
+		md.update(text.getBytes());
+
+		byte[] digest = md.digest();
+		StringBuffer sb = new StringBuffer();
+		for (byte b : digest) {
+			sb.append(String.format("%02x", b & 0xff));
+		}
+		return sb.toString();
+	}
+
+	public static String generateToken() {
 		SecureRandom random = new SecureRandom();
-		String text = new BigInteger(130, random).toString(32);
+		String text = new BigInteger(130, random).toString(10);
 		return text;
 	}
 
